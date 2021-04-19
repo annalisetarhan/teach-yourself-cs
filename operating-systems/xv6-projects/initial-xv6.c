5c94bca55d15d391ed85b2c27a45a1ffeb859852
Annalise Tarhan <annalisetarhan@gmail.com>
2021-04-19 14:13:48 +0300

    Fix initial-xv6

--- operating-systems/xv6-projects/initial-xv6.c
+++ operating-systems/xv6-projects/initial-xv6.c
@@ -5,18 +5,20 @@
 //
 
 /*
- Implementing getreadcount() requires small changes across six files.
+ Implementing getreadcount() requires small changes across seven files.
  
- The first step is to update the kernel to track the readcount, which
- is accomplished by adding a readcount variable to the cpu struct and
- then updating sys_read() to increment the cpu's readcount each time
- it is called. pushcli() and popcli() are used to disable interrupts
- in order to avoid deadlock while interacting with the cpu struct.
+ The first step is to update the kernel to track the readcount by adding
+ a readcount variable to the cpu struct and then updating sys_read() to
+ increment the cpu's readcount each time it is called. pushcli() and
+ popcli() are used to disable interrupts in order to avoid deadlock while
+ interacting with the cpu struct.
  
  Then, add a system function, sys_getreadcount(), to provide access to
  the cpu's readcount. The function itself goes in sysproc.c, but small
  additions to syscall.h, syscall.c, and usys.S are required for a user
  to be able to request that the kernel call the function on their behalf.
+ 
+ Finally, add a function prototype for the user function in user.h.
  */
 
 /* proc.h */
@@ -29,19 +31,11 @@ struct cpu {
 int
 sys_read(void)
 {
-  struct file *f;
-  int n;
-  char *p;
-    
-    /* Begin */
+    // ...
     pushcli();
     mycpu()->readcount++;
     popcli();
-    /* End */
-    
-  if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
-    return -1;
-  return fileread(f, p, n);
+    // ...
 }
 
 /* sysproc.c */
@@ -69,3 +63,5 @@ static int (*syscalls[])(void) = {
 /* usys.S */
 SYSCALL(getreadcount)
 
+/* user.h */
+int getreadcount(void);
